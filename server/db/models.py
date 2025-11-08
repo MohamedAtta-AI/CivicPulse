19cc88b6f2274a1121195b4bd88d8ed36077cc0b
Mohamed Atta <M.TarekSaad@nu.edu.eg>
2025-11-08 23:48:49 +0200

    post and comment

--- server/db/models.py
+++ server/db/models.py
@@ -4,9 +4,19 @@ from sqlmodel import SQLModel, Field, Relationship
 from uuid import UUID, uuid4
 
 
-class DataPoint(SQLModel, table=True):
-    post_id: UUID = Field(default_factory=uuid4, primary_key=True)
+class Post(SQLModel, table=True):
+    id: UUID = Field(default_factory=uuid4, primary_key=True)
     created_at: datetime.datetime
     content: str
-    content_type: Literal["post", "comment"]
     source: str
+    url: str
+    score: int
+    comments: list["Comment"] = Relationship(back_populates="post")
+
+
+class Comment(SQLModel, table=True):
+    id: UUID = Field(default_factory=uuid4, primary_key=True)
+    created_at: datetime.datetime
+    content: str
+    post_id: UUID = Field(foreign_key="post.id")
+    post: Post = Relationship(back_populates="comments")
