b20e62acc4f076216f8fa5e326489f2d7830c30c
Mohamed Atta <M.TarekSaad@nu.edu.eg>
2025-11-08 20:21:37 +0200

    backend initial structure

--- server/core/config.py
+++ server/core/config.py
@@ -1,18 +1,24 @@
-from pydantic import BaseSettings, AnyUrl
+from typing import Literal
+from functools import lru_cache
+from pydantic import BaseSettings, Field, AnyUrl
 
 
 class Settings(BaseSettings):
-    APP_NAME="CivicPulse"
-    ENV: str = "dev"
+    APP_NAME: str = Field(default="CivicPulse", env="APP_NAME")
+    PROD: bool = Field(default=False, env="PROD")
+    DB_USER: str = Field(..., env="DB_USER")
+    DB_PASS: str = Field(..., env="DB_PASS")
+    DB_HOST: str = Field(..., env="DB_HOST")
+    DB_PORT: int = Field(..., env="DB_PORT")
+    DB_NAME: str = Field(default=5432, env="DB_NAME")
 
-    DATABASE_URL: AnyUrl = "postgresql://postgres:postgres@db:5432/mediapulse"
 
     class Config:
         env_file = ".env"
 
     @property
-    def db_url(self):
-        return f"postgresql+psycopg2://{self.db_user}:{self.db_pass}@{self.db_host}:{self.db_port}/{self.db_name}"
+    def db_url(self) -> str:
+        return f"postgresql+psycopg2://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
 
 
 @lru_cache()
